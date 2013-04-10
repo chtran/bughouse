878f0ee387d4cb8390850189157feb963f3ae551
mp42 <melpak92@yahoo.com>
2013-04-10 02:38:16 -0400

    BughouseGUI contains a private class Board which graphically represents the chessboard

--- src/edu/brown/cs32/bughouse/global/BughouseGUI.java
+++ src/edu/brown/cs32/bughouse/global/BughouseGUI.java
@@ -1,17 +1,22 @@
 package edu.brown.cs32.bughouse.global;
 
 import java.awt.*;
-
 import javax.swing.*;
-
 import edu.brown.cs32.bughouse.interfaces.FrontEnd;
 
+/**
+ * 
+ * @author mp42
+ *	BughouseGUI is a JFrame that holds all the different menus of in Bughouse.
+ *	Each player only sees one JFrame with different menus implemented as JPanels
+ *	laid out with the CardLayout manager. This is the only GUI class that should
+ * 	expose methods to other logic classes. 
+ */
+
 public class BughouseGUI extends JFrame implements FrontEnd{
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
+	private Board userBoard_, otherBoard_;
 
 	public BughouseGUI(){
 		super("Bughouse Chess");
@@ -33,39 +38,15 @@ public class BughouseGUI extends JFrame implements FrontEnd{
 		game.add(createBoard(),BorderLayout.CENTER);
 		game.add(new JLabel(" This is the options panel showing information"), BorderLayout.EAST);
 		game.add(new JLabel("This is to show pieces that are available to the player to put down"), BorderLayout.SOUTH);
-		//game.add(new JLabel(" "), BorderLayout.NORTH);
 		return game;
 	}
 	
 	private JComponent createBoard(){
 		JTabbedPane boardContainer = new JTabbedPane();
-		JPanel board = new JPanel(new GridLayout(8,8,1,0));
-		board.setPreferredSize(new Dimension(400,400));
-		Color current = Color.GRAY;
-		for (int i = 0; i<8;i++){
-			for (int j = 0; j<8;j++){
-				JPanel box = new JPanel();
-				if (current == Color.GRAY){
-					box.setBackground(Color.WHITE);
-					current = Color.WHITE;
-				}
-				else {
-					box.setBackground(Color.GRAY);
-					current = Color.GRAY;
-				}
-				box.setBorder(null);
-				board.add(box);
-			}
-			if (current == Color.GRAY){
-				current = Color.WHITE;
-			}
-			else {
-				current = Color.GRAY;
-			}
-			
-		}
-		boardContainer.addTab("Your Game", board);
-		boardContainer.addTab("Other Game", new JLabel("This is the other board"));
+		userBoard_ = new Board();
+		boardContainer.addTab("Your Game", userBoard_);
+		otherBoard_ = new Board();
+		boardContainer.addTab("Other Game", otherBoard_);
 		return boardContainer;
 	}
 	
@@ -76,6 +57,50 @@ public class BughouseGUI extends JFrame implements FrontEnd{
 	@Override
 	public void showEndGameMessage() {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	/**
+	 * 
+	 * @author mp42
+	 *	Board class which graphically represents the a current 
+	 *	game with pieces and players. A private class since no 
+	 *	other class except BughouseGUI needs to know about it - might be implemented
+	 *	as its own class in the future for separation between JFrame and its content
+	 */
+	
+	private class Board extends JPanel {
+		
+		private static final long serialVersionUID = 1L;
+
+		public Board() {
+			super(new GridLayout(8,8,1,0));
+			this.setPreferredSize(new Dimension(400,400));
+			Color current = Color.GRAY;
+			for (int i = 0; i<8;i++){
+				for (int j = 0; j<8;j++){
+					JPanel box = new JPanel();
+					if (current == Color.GRAY){
+						box.setBackground(Color.WHITE);
+						current = Color.WHITE;
+					}
+					else {
+						box.setBackground(Color.GRAY);
+						current = Color.GRAY;
+					}
+					box.setBorder(null);
+					this.add(box);
+				}
+				if (current == Color.GRAY){
+					current = Color.WHITE;
+				}
+				else {
+					current = Color.GRAY;
+				}
+				
+			}
+		}
 		
 	}
 	
