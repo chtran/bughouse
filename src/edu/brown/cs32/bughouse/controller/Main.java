28e1fc44f6662be7cbf3d7ff7f1b5e04a15f1f84
Chau Tran <chau_tran@brown.edu>
2013-04-25 19:04:23 -0400

    Closing scanner

--- src/edu/brown/cs32/bughouse/controller/Main.java
+++ src/edu/brown/cs32/bughouse/controller/Main.java
@@ -9,12 +9,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO don't have this hard coded
 		int port = 3333;
-
+		Scanner scanner = new Scanner(System.in);
 		try {
 			BughouseServer server = new BughouseServer(port);
 
 			// Listen for any commandline input; quit on "exit" or emptyline
-			Scanner scanner = new Scanner(System.in);
 			String line = null;
 			while (scanner.hasNextLine()) {
 				line = scanner.nextLine();
@@ -25,6 +24,8 @@ public class Main {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			scanner.close();
 		}
 	}
 }
